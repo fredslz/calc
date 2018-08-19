3390819df4d6130267ebd9ec4f8694a897f464e1
administrador <fredslz@gmail.com>
2018-08-19 20:59:38 -0300

    tests

--- test_pytestes.py
+++ test_pytestes.py
@@ -6,9 +6,9 @@ from app import soma, is_number
 def test_soma(a,b, expected):
     assert soma(a,b) == expected
 
-# @pytest.mark.parametrize('a,expected', [(0, 0), (-2,-4), (5,10), (7.5, 15), (-1.3, -2.6), ('Fred', None)])
-# def test_dobro(a,expected):
-#     assert soma(a) == expected
+@pytest.mark.parametrize('a,expected', [(0, 0), (-2,-4), (5,10), (7.5, 15), (-1.3, -2.6), ('Fred', None)])
+def test_dobro(a,expected):
+    assert soma(a) == expected
 
 @pytest.mark.parametrize('num,expected', [(0, True), (-1,True), (1.5,True), (-7.5, True), ('-7.5', True), ('1', True), ('2e3', True), ('Fred', False), ('7*5', False), ('1..2', False)])
 def test_is_number(num,expected):
